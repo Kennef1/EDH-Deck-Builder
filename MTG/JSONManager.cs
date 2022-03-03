a6cb1bae33336743754914996b18d2b4c13876f3
Kennef1 <100816308+Kennef1@users.noreply.github.com>
2022-03-03 15:17:48 +1300

    Add files via upload

---
+++ MTG/JSONManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deck_Builder.MTG;
+
+namespace Deck_Builder.MTG
+{
+    static public class JSONManager
+    {
+        //CONSTRUCTOR
+
+
+        //MEMBERS
+
+
+        //METHODS
+        /*convert list to json file*/
+
+        /*pull relevant details from json master file and save to MTG_Card object List*/
+        public static void pull_data(List<MTG_Card> list)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog.Filter = "json files (*.json)|*.json|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.ShowDialog();
+            string s_Directory = openFileDialog.FileName;
+
+
+        }
+
+        /*convert json file to list*/
+    }
+}
---
+++ MTG/MTG_Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck_Builder.MTG
+{
+    public class MTG_Card
+    {
+        //CONSTRUCTOR
+        public MTG_Card(string n)
+        {
+            s_Name = n;
+            b_isCommander = false;
+        }
+
+        //MEMBERS
+        public string s_Name;
+        public bool b_isCommander;
+
+        //METHODS
+    }
+}
